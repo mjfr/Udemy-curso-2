bc438440679577a8cad0cc5540fb8130ea9f99d8
Matheus José Felipe Rosa <pwvmat@gmail.com>
2022-01-29 00:34:24 -0300

    Dia 11.1 - Início o Blackjack simplificado

---
+++ Seção 11; Day 11 - Beginner - The Blackjack Capstone Project/109. Blackjack Program Requirements and Game Rules.py
@@ -0,0 +1,112 @@
+############### Blackjack Project #####################
+
+#Difficulty Normal 😎: Use all Hints below to complete the project.
+#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
+#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
+#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+##################### Hints #####################
+
+#Hint 1: Go to this website and try out the Blackjack game: 
+#   https://games.washingtonpost.com/games/blackjack/
+#Then try out the completed Blackjack project here: 
+#   http://blackjack-final.appbrewery.repl.run
+
+#Hint 2: Read this breakdown of program requirements: 
+#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
+#Then try to create your own flowchart for the program.
+
+#Hint 3: Download and read this flow chart I've created: 
+#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
+
+#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
+#11 is the Ace.
+#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
+#user_cards = []
+#computer_cards = []
+
+#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
+#and returns the score. 
+#Look up the sum() function to help you do this.
+
+#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+
+#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+
+#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+
+#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+
+#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
+
+#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
+
+#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
+
+#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+
+import art
+from os import system
+import random
+
+def main():
+
+    play_blackjack = input("Start Blackjack game? Type 'y' to play or 'n' to exit.\nAnswer: ")
+    system("cls")
+    # Ás, 2, [...], 9, 10, J, Q, K
+    card_deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    player_dealer_score = {"player_score" : 0, "dealer_score" : 0}
+    player_dealer_deck = {"player_deck" : [], "dealer_deck" : []}
+
+
+    def first_card_assignment():
+        if player_dealer_deck["dealer_deck"] == [] and player_dealer_deck["player_deck"] == []:
+            while len(player_dealer_deck["dealer_deck"]) < 2 and len(player_dealer_deck["player_deck"]) < 2:
+                player_dealer_deck["dealer_deck"].append(random.choice(card_deck))
+                player_dealer_deck["player_deck"].append(random.choice(card_deck))
+
+    
+    def score_assignment():
+        for card in player_dealer_deck["player_deck"]:
+            player_dealer_score["player_score"] += card
+        for card in player_dealer_deck["dealer_deck"]:
+            player_dealer_score["dealer_score"] += card
+
+        
+    def hit(player_dealer_deck):
+        player_dealer_deck.append(random.choice(card_deck))
+
+
+    def blackjack():
+        if play_blackjack == "y":
+            while play_blackjack == "y":
+                print(art.logo)
+                first_card_assignment()
+                score_assignment()
+                print(f"Your cards are: {player_dealer_deck['player_deck']} and your current score is {player_dealer_score['player_score']}\nDealer's first card is [{player_dealer_deck['dealer_deck'][0]}]")
+                # hit_pass = input("\nType 'h' to get another card (hit) or type 'p' to pass\nAnswer: ").lower()
+                # if hit_pass == "h":
+                #     hit(player_dealer_deck["player_deck"])
+
+
+    blackjack()
+    print(player_dealer_deck)
+    print(player_dealer_score)
+            
+
+if __name__ == '__main__':
+    main()
---
+++ Seção 11; Day 11 - Beginner - The Blackjack Capstone Project/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+    print(player_dealer_deck)
+    print(player_dealer_score)
