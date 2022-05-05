0d380d1d5ca3265b1285da2e6346dc060c37215c
Matheus José Felipe Rosa <pwvmat@gmail.com>
2022-05-05 20:47:27 -0300

    Dia 66 - (1/2) Fazendo a minha própria API. GET e POST implementados. Falta PUT/PATCH e DELETE, além da documentação da API

---
+++ Seção 66; Day 66 - Advanced - Building Your Own API with RESTful Routing/main.py
@@ -0,0 +1,100 @@
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from random import choice
+
+app = Flask(__name__)
+
+##Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+
+## HTTP GET - Read Record
+@app.route("/random")
+def get_random():
+    cafes = Cafe.query.all()
+    cafe = choice(cafes)
+    return jsonify(cafe=cafe.to_dict())
+    # A way to convert a dictionary to json using jsonify and above in the "Cafe" class, is another way
+    # return jsonify(cafe={
+    #     "id": cafe.id,
+    #     "name": cafe.name,
+    #     "map_url": cafe.map_url,
+    #     "image": cafe.img_url,
+    #     "location": cafe.location,
+    #     "has_sockets": cafe.has_sockets,
+    #     "has_toilet": cafe.has_toilet,
+    #     "has_wifi": cafe.has_wifi,
+    #     "can_take_calls": cafe.can_take_calls,
+    #     "seats": cafe.seats,
+    #     "coffee_price": cafe.coffee_price
+    # })
+
+
+@app.route("/all")
+def get_all():
+    cafes = Cafe.query.all()
+    cafes_dict = [cafe.to_dict() for cafe in cafes]
+    return jsonify(cafe=cafes_dict)
+
+
+@app.route("/search")
+def search():
+    cafes = Cafe.query.filter_by(location=request.args.get("loc"))
+    cafes_dict = [cafe.to_dict() for cafe in cafes]
+    if len(cafes_dict) == 0:
+        return jsonify(response={"Not Found": "Sorry, we don't have a cafe at that location."})
+    return jsonify(cafes_dict)
+
+
+## HTTP POST - Create Record
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    cafe = Cafe()
+    cafe.name = request.form.get("name")
+    cafe.map_url = request.form.get("map_url")
+    cafe.img_url = request.form.get("img_url")
+    cafe.location = request.form.get("location")
+    cafe.seats = request.form.get("seats")
+    cafe.has_toilet = bool(request.form.get("has_toilet"))
+    cafe.has_wifi = bool(request.form.get("has_wifi"))
+    cafe.has_sockets = bool(request.form.get("has_sockets"))
+    cafe.can_take_calls = bool(request.form.get("can_take_calls"))
+    cafe.coffee_price = request.form.get("coffee_price")
+    if Cafe.query.filter_by(name=request.form.get("name")).first() is None:
+        db.session.add(cafe)
+        db.session.commit()
+        return jsonify(response={"success": "Successfully added the new cafe."})
+    return jsonify(response={"Already Exists": "The cafe you're trying to add already exists in the database."})
+
+## HTTP PUT/PATCH - Update Record
+
+## HTTP DELETE - Delete Record
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
