a77abfd2e016f5232c9fae6e9e414af80af10aba
Matheus José Felipe Rosa <pwvmat@gmail.com>
2022-05-06 16:25:57 -0300

    Dia 66 - (2/2) Subindo os métodos PATCH e DELETE que faltavam. No index.html está o link para a documentação da API

--- Seção 66; Day 66 - Advanced - Building Your Own API with RESTful Routing/main.py
+++ Seção 66; Day 66 - Advanced - Building Your Own API with RESTful Routing/main.py
@@ -4,13 +4,13 @@ from random import choice
 
 app = Flask(__name__)
 
-##Connect to Database
+# Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-##Cafe TABLE Configuration
+# Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -33,12 +33,12 @@ def home():
     return render_template("index.html")
     
 
-## HTTP GET - Read Record
+# HTTP GET - Read Record
 @app.route("/random")
 def get_random():
     cafes = Cafe.query.all()
     cafe = choice(cafes)
-    return jsonify(cafe=cafe.to_dict())
+    return jsonify(cafe=cafe.to_dict()), 200
     # A way to convert a dictionary to json using jsonify and above in the "Cafe" class, is another way
     # return jsonify(cafe={
     #     "id": cafe.id,
@@ -59,7 +59,7 @@ def get_random():
 def get_all():
     cafes = Cafe.query.all()
     cafes_dict = [cafe.to_dict() for cafe in cafes]
-    return jsonify(cafe=cafes_dict)
+    return jsonify(cafe=cafes_dict), 200
 
 
 @app.route("/search")
@@ -67,11 +67,11 @@ def search():
     cafes = Cafe.query.filter_by(location=request.args.get("loc"))
     cafes_dict = [cafe.to_dict() for cafe in cafes]
     if len(cafes_dict) == 0:
-        return jsonify(response={"Not Found": "Sorry, we don't have a cafe at that location."})
-    return jsonify(cafes_dict)
+        return jsonify(response={"Not Found": "Sorry, we don't have a cafe at that location."}), 404
+    return jsonify(cafes_dict), 200
 
 
-## HTTP POST - Create Record
+# HTTP POST - Create Record
 @app.route("/add", methods=["GET", "POST"])
 def add():
     cafe = Cafe()
@@ -88,12 +88,34 @@ def add():
     if Cafe.query.filter_by(name=request.form.get("name")).first() is None:
         db.session.add(cafe)
         db.session.commit()
-        return jsonify(response={"success": "Successfully added the new cafe."})
-    return jsonify(response={"Already Exists": "The cafe you're trying to add already exists in the database."})
+        return jsonify(response={"Ok": "Successfully added the new cafe."}), 200
+    return jsonify(response={"Conflict": "The cafe you're trying to add already exists in the database."}), 409
 
-## HTTP PUT/PATCH - Update Record
 
-## HTTP DELETE - Delete Record
+# HTTP PUT/PATCH - Update Record
+@app.route("/update-price/<cafe_id>", methods=["PATCH"])
+def update_price(cafe_id):
+    cafe = Cafe.query.filter_by(id=cafe_id).first()
+    coffee_price = request.args.get("new_price")
+    if cafe is not None:
+        cafe.coffee_price = coffee_price
+        db.session.commit()
+        return jsonify(response={"Ok": "Successfully updated the price."}), 200
+    return jsonify(response={"Not Found": "The price was not updated. Please try again using a valid id."}), 404
+
+
+# HTTP DELETE - Delete Record
+@app.route("/report-closed/<cafe_id>", methods=["DELETE"])
+def delete(cafe_id):
+    api_key = "TopSecretAPIKey"
+    cafe = Cafe.query.filter_by(id=cafe_id).first()
+    if request.args.get("api_key") == api_key:
+        if cafe is not None:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"Ok": "Successfully deleted the cafe."}), 200
+        return jsonify(response={"Not Found": "The cafe id was not found. No cafe was deleted."}), 404
+    return jsonify(response={"Forbidden": "You don't have permission to delete the cafe."}), 403
 
 
 if __name__ == '__main__':
