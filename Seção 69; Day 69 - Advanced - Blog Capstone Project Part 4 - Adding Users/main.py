2b5fe7c154215a98f4fb6f7debef62a74c54e3c2
Matheus José Felipe Rosa <pwvmat@gmail.com>
2022-05-16 20:51:39 -0300

    Dia 69 - Commit final!!!! Utilizando Heroku Postgres, e requirements.txt atualizado.

--- Seção 69; Day 69 - Advanced - Blog Capstone Project Part 4 - Adding Users/main.py
+++ Seção 69; Day 69 - Advanced - Blog Capstone Project Part 4 - Adding Users/main.py
@@ -1,3 +1,4 @@
+import os
 from functools import wraps
 from flask import Flask, render_template, redirect, url_for, flash, request, abort
 from flask_bootstrap import Bootstrap
@@ -10,12 +11,14 @@ from forms import CreatePostForm, RegisterUserForm, LoginUserForm, CommentForm
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
