dda13478777cc69ba1f35d1712bc1d3d5454c6c9
Matheus José Felipe Rosa <pwvmat@gmail.com>
2022-06-15 18:55:32 -0300

    Dia 82 - A base geral está pronta, ao decorrer dos próximos projetos irei adicionando eles à area de portfólio do site e também será adicionado o currículo depois

--- Seção 82; Day 82 - Professional Portfolio Project - [Python Web Development]/main.py
+++ Seção 82; Day 82 - Professional Portfolio Project - [Python Web Development]/main.py
@@ -1,16 +1,22 @@
 # TODO - 1: Make a website to show off my skills
 # This is the second portfolio project from the course.
 
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, send_from_directory
 from datetime import date
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "./static/files/"
 
 
 @app.route("/")
 def home():
     year = date.today().year
     return render_template("index.html", year=year)
+
+
+@app.route("/curriculum")
+def cv_download():
+    return send_from_directory(directory=app.config["UPLOAD_FOLDER"], path="Blender-Keyboard-Shortcuts.pdf")
 
 
 if __name__ == "__main__":
