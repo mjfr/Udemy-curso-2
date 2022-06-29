a2fe3363dc399f6ae0a1dda1f21e870559bff2f2
Matheus José Felipe Rosa <pwvmat@gmail.com>
2022-06-29 20:11:22 -0300

    Dia 85 - Finalizado. Entry passou a ser Text para aceitar mudança de cor (em caso de letra errada); Erros contabilizando corretamente; Não trava mais para digitar rapidamente!

--- Seção 85; Day 85 - Professional Portfolio Project - [GUI]/main.py
+++ Seção 85; Day 85 - Professional Portfolio Project - [GUI]/main.py
@@ -3,12 +3,21 @@ import tkinter as tk
 from essential_generators import DocumentGenerator
 
 
+def gen_random_sentence():
+    dg = DocumentGenerator()
+    sentence = dg.sentence()
+    while len(sentence) < 150:
+        sentence += f" {dg.sentence()}"
+    sentence = sentence.replace("\n", " ").replace("–", "-").replace("—", "-")
+    return sentence
+
+
 class App(tk.Tk):
     def __init__(self):
         super(App, self).__init__()
         self.title("Furious Fingers")
         self.resizable(False, False)
-        self.text_entry = None
+        self.text = None
         self.text_label = None
         self.timer = None
         self.start_timer_btn = None
@@ -19,22 +28,25 @@ class App(tk.Tk):
         self.total_characters = 0
         self.total_errors = 0
         self.uncorrected_errors = 0
-        self.sentence = self.gen_random_sentence()
         self.create_widgets()
+        self.sentence = gen_random_sentence()
         self.gross_wps = 0
         self.net_wps = 0
         self.correct_characters = 0
+        self.current_correct_characters = 0
         self.accuracy = 0
+        self.err_index = []
 
     def place_text(self):
         self.text_label = tk.Label(text="", font=("Segoe UI", 12, "normal"), wraplength=500)
         self.text_label.grid(column=0, row=1, pady=15, columnspan=3)
 
-    def place_entry(self):
-        self.text_entry = tk.Entry(width=100)
-        self.text_entry.grid(column=0, row=2, pady=15, padx=30, columnspan=3)
-        self.text_entry["state"] = "disabled"
-        self.text_entry.bind("<KeyPress>", self.match_words)
+    def place_textbox(self):
+        self.text = tk.Text(width=70, height=5)
+        self.text.grid(column=0, row=2, pady=15, padx=30, columnspan=3)
+        self.text["state"] = "disabled"
+        self.text.bind("<Return>", lambda e: "break")
+        self.text.bind("<KeyRelease>", self.match_words)
 
     def place_timer(self):
         self.timer = tk.Label(text="Remaining time: 60 seconds", justify="center", foreground="red")
@@ -44,105 +56,88 @@ class App(tk.Tk):
         self.start_timer_btn = tk.Button(text="Start", command=self.start_timer)
         self.start_timer_btn.grid(column=0, row=0)
         self.reset_btn = tk.Button(text="Reset", command=self.reset)
+        self.reset_btn.bind("<Button-1>", lambda e: self.text_label.configure(text="Generating new text, please wait."))
         self.reset_btn.grid(column=2, row=0)
 
     def start_timer(self):
         self.after_aux = self.after(500, self.update_timer)
 
     def update_timer(self):
-        if self.time > 0:
+        if self.sentence == self.text.get("1.0", "end-1c"):
+            self.after_cancel(self.after_aux)
+            self.show_statistics()
+        elif self.time > 0:
             if self.time > 60:
                 self.text_label.configure(text=f"Get prepared! Text appearing in {self.time - 60} seconds")
             else:
-                # self.text_label.configure(text=self.sentence["sentence"])
                 self.text_label.configure(text=self.sentence)
-                self.text_entry["state"] = "normal"
-                self.text_entry.focus_set()
+                self.text["state"] = "normal"
+                self.text.focus_set()
             self.after_aux = self.after(500, self.start_timer)
             self.time -= 1
             if self.time <= 60:
                 self.timer.configure(text=f"Remaining time: {self.time} seconds")
         else:
-            self.text_entry["state"] = "disabled"
-            self.gross_wps = self.calc_gross_wpm()
-            self.net_wps = self.calc_net_wpm()
-            self.correct_characters = self.calc_correct_characters()
-            self.accuracy = self.calc_accuracy()
-            self.text_label.configure(text=f"Your stats are:\n"
-                                           f"Gross Words per Second: {self.gross_wps:.4};\n"
-                                           f"Net Words per Second: {self.net_wps:.4};\n"
-                                           f"Correct Characters: {self.correct_characters}/{self.typed_entries};\n"
-                                           f"Correct Characters: {self.correct_characters}/{len(self.sentence)};\n"
-                                           f"With an accuracy of: {self.accuracy:.4}%")
+            self.show_statistics()
+
+    def show_statistics(self):
+        self.text["state"] = "disabled"
+        self.gross_wps = self.calc_gross_wpm()
+        self.net_wps = self.calc_net_wpm()
+        self.correct_characters = self.calc_correct_characters()
+        self.accuracy = self.calc_accuracy()
+        self.current_correct_characters = self.calc_current_correct_characters()
+        self.total_characters = len(self.sentence)
+        self.text_label.configure(
+            text=f"Your stats are:\n"
+                 f"Gross Words per Second: {self.gross_wps:.4}\n"
+                 f"Net Words per Second: {self.net_wps:.4}\n"
+                 f"Gross Words per Minute: {self.gross_wps * 60:.4}\n"
+                 f"Net Words per Minute: {self.net_wps * 60:.4}\n"
+                 f"Correct/Typed Characters: {self.correct_characters}/{self.typed_entries}\n"
+                 f"Correct/Total Characters: {self.current_correct_characters}/{self.total_characters}\n"
+                 f"With an accuracy of: {self.accuracy:.4}%"
+        )
 
     def reset(self):
-        self.after_cancel(self.after_aux)
+        try:
+            self.after_cancel(self.after_aux)
+        except ValueError:
+            pass
         self.time = 63
         self.timer.configure(text="Remaining time: 60 seconds")
-        self.sentence = self.gen_random_sentence()
+        self.sentence = gen_random_sentence()
         self.text_label.configure(text="")
-        self.text_entry["state"] = "normal"
-        self.text_entry.delete(0, "end")
-        self.text_entry["state"] = "disabled"
+        self.text["state"] = "normal"
+        self.text.delete("1.0", "end-1c")
+        self.text["state"] = "disabled"
         self.typed_entries = 0
         self.total_characters = 0
         self.total_errors = 0
         self.uncorrected_errors = 0
+        self.current_correct_characters = 0
 
     def create_widgets(self):
         self.place_timer()
         self.place_text()
-        self.place_entry()
+        self.place_textbox()
         self.place_buttons()
 
-    def gen_random_sentence(self):
-        dg = DocumentGenerator()
-        sentence = dg.sentence()
-        while len(sentence) < 300:
-            sentence += f" {dg.sentence()}"
-        sentence = sentence.replace("\n", " ").replace("–", "-").replace("—", "-")
-        # sentence_list = [character for character in sentence]
-        self.total_characters = len(sentence)
-        # return {"sentence": sentence, "letters": sentence_list}
-        return sentence
-
     def match_words(self, event):
-        # if last_typed is None or last_typed == "":
-        #     last_typed = self.text_entry.get()
-        # else:
-        #     last_typed = self.text_entry.get()
-
-        # try:
-        #     last_typed = self.text_entry.get()
-        #     print(repr(last_typed))
-        #     # last_typed = self.text_entry.get()[-1]
-        # except IndexError:
-        #     pass
-
-        if event.char and event.char != "\t" and event.char != "\r" and event.char != "\n" and event.char != "\x1b" \
-                and event.char != "\x08":
-            last_typed = self.text_entry.get()
+        if event.char and event.char != "\t" and event.char != "\x1b" and event.char != "\x08" \
+                and self.text["state"] == "normal":
+            last_typed = self.text.get("1.0", "end-1c")
             if last_typed is None or last_typed == "":
                 last_typed = event.char
             self.typed_entries += 1
-            # print(event)
-            err_index = [self.add_error(i, letter1 != letter2) for i, (letter1, letter2) in enumerate(zip(self.sentence, last_typed)) if letter1 != letter2]
-            self.uncorrected_errors = len(err_index)
-            print(self.total_errors)
-            print(last_typed)
-            print(err_index)
-            # Gera erro quando acaba, talvez fazer um try exception para levar à contagem dos acertos, etc.
-            # last_character = self.sentence["letters"][0]
-            # if last_character == last_typed:
-            #     current_character = self.sentence["letters"].pop(self.sentence["letters"].index(last_character))
-            #     print(last_character)
-            #     print(self.sentence["letters"])
-            # else:
-            #     print("Não deu match")
-            #     self.total_errors += 1
+            self.err_index = [self.add_error(i) for i, (letter1, letter2) in
+                              enumerate(zip(self.sentence, last_typed)) if letter1 != letter2]
+            self.uncorrected_errors = len(self.err_index)
 
-    def add_error(self, index: int, condition: bool):
-        if condition:
+    def add_error(self, index: int):
+        self.text.tag_config("error", foreground="red")
+        self.text.tag_add("error", f"1.{index}", f"1.{index+1}")
+        if not self.err_index.count(index):
             self.total_errors += 1
         return index
 
@@ -154,6 +149,12 @@ class App(tk.Tk):
 
     def calc_correct_characters(self):
         return self.typed_entries - self.total_errors
+
+    def calc_current_correct_characters(self):
+        self.text["state"] = "normal"
+        correct_characters_count = len(self.text.get("1.0", "end-1c")) - self.uncorrected_errors
+        self.text["state"] = "disabled"
+        return correct_characters_count
 
     def calc_accuracy(self):
         return self.correct_characters / self.typed_entries * 100
