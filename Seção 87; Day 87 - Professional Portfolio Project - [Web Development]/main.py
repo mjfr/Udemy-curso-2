4b70a60ef62c5ce9b74e443812c9fc44b3d339cd
Matheus José Felipe Rosa <pwvmat@gmail.com>
2022-07-11 20:54:31 -0300

    Dia 87 - Finalizado. Voltei a seguir a proposta original de fazer apenas um website (não um pseudo webservice como estava fazendo antes. Dessa forma ficou bem simples tendo apenas o CRUD.

--- Seção 87; Day 87 - Professional Portfolio Project - [Web Development]/main.py
+++ Seção 87; Day 87 - Professional Portfolio Project - [Web Development]/main.py
@@ -1,18 +1,20 @@
 # TODO-1: Build a website that lists cafes with wifi and power for remote working.
 import os
 import wtforms.validators
-from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, URLField, TimeField, SelectField, SubmitField, BooleanField, IntegerField, FloatField
+from wtforms import StringField, URLField, SubmitField, BooleanField, IntegerField, FloatField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
-from random import choice
+from datetime import date
 
+year = date.today().year
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafes.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.add_template_global(name="YEAR", f=year)
 Bootstrap(app)
 db = SQLAlchemy(app)
 
@@ -22,7 +24,7 @@ class Cafe(db.Model):
     name = db.Column(db.String(100), unique=True, nullable=False)
     map_url = db.Column(db.String(400), unique=True, nullable=False)
     img_url = db.Column(db.String(400), unique=True, nullable=False)
-    location = db.Column(db.String(250), unique=True, nullable=False)
+    city = db.Column(db.String(58), nullable=False)
     seats = db.Column(db.Integer, nullable=False)
     has_toilet = db.Column(db.Boolean, nullable=False)
     has_wifi = db.Column(db.Boolean, nullable=False)
@@ -42,7 +44,7 @@ class CafeForm(FlaskForm):
                                                                wtforms.validators.URL(message="Use a valid URL.")])
     img_url = URLField(label="URL de uma Foto do Café", validators=[DataRequired(),
                                                                     wtforms.validators.URL(message="Use a valid URL.")])
-    location = StringField(label="Endereço do Café", validators=[DataRequired()])
+    city = StringField(label="Cidade do Café", validators=[DataRequired()])
     seats = IntegerField(label="Número de Acentos", validators=[DataRequired()])
     has_toilet = BooleanField(label="Tem Banheiro?")
     has_wifi = BooleanField(label="Tem WiFi?")
@@ -61,64 +63,22 @@ def get_all():
     return [cafe.to_dict() for cafe in cafes]
 
 
-@app.route("/api/all")
-def get_all_json():
-    cafes_dict = get_all()
-    return jsonify(cafe=cafes_dict), 200
-
-
 @app.route("/web/all")
-def get_all_html():
+def get_all():
     cafes_dict = get_all()
     print(cafes_dict)
     return render_template("cafes.html", cafes=cafes_dict)
 
 
-@app.route("/search")
-def search():
-    cafes = Cafe.query.filter_by(location=request.args.get("location"))
-    cafes_dict = [cafe.to_dict() for cafe in cafes]
-    if len(cafes_dict) == 0:
-        return jsonify(response={"Not Found": "Sorry, we don't have a cafe at that location."}), 404
-    return jsonify(cafes_dict), 200
-
-
-@app.route("/random")
-def get_random():
-    cafe = choice(Cafe.query.all())
-    return jsonify(cafe=cafe.to_dict()), 200
-
-
-@app.route("/api/add", methods=["POST"])
-def add_cafe_api():
-    form = CafeForm()
-    if form.validate_on_submit():
-        cafe = Cafe()
-        cafe.name = request.args.get("name")
-        cafe.map_url = request.args.get("map_url")
-        cafe.img_url = request.args.get("img_url")
-        cafe.location = request.args.get("location")
-        cafe.seats = request.args.get("seats")
-        cafe.has_toilet = request.args.get("has_toilet")
-        cafe.has_wifi = request.args.get("has_wifi")
-        cafe.has_sockets = request.args.get("has_sockets")
-        cafe.coffee_price = request.args.get("coffee_price")
-        if Cafe.query.filter_by(name=cafe.name).first() is None:
-            db.session.add(cafe)
-            db.session.commit()
-            return jsonify(response={"Ok": "Successfully added the new Cafe."}), 200
-        return jsonify(response={"Conflict": "The cafe you're trying to add already exists in the database."}), 409
-
-
 @app.route("/web/add", methods=["GET", "POST"])
-def add_cafe_html():
+def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
         cafe = Cafe()
         cafe.name = request.form.get("name")
         cafe.map_url = request.form.get("map_url")
         cafe.img_url = request.form.get("img_url")
-        cafe.location = request.form.get("location")
+        cafe.city = request.form.get("city")
         cafe.seats = request.form.get("seats")
         cafe.has_toilet = bool(request.form.get("has_toilet"))
         cafe.has_wifi = bool(request.form.get("has_wifi"))
@@ -127,18 +87,18 @@ def add_cafe_html():
         if Cafe.query.filter_by(name=cafe.name).first() is None:
             db.session.add(cafe)
             db.session.commit()
-            return redirect(url_for("get_all_html"))
+            return redirect(url_for("get_all"))
     return render_template("add_cafe.html", form=form)
 
 
-@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+@app.route("/update-price/<int:cafe_id>", methods=["GET", "POST"])
 def update_price(cafe_id):
     cafe = Cafe.query.filter_by(id=cafe_id).first()
     edit_form = CafeForm(
         name=cafe.name,
         map_url=cafe.map_url,
         img_url=cafe.img_url,
-        location=cafe.location,
+        city=cafe.city,
         seats=cafe.seats,
         has_toilet=cafe.has_toilet,
         has_wifi=cafe.has_wifi,
@@ -149,15 +109,15 @@ def update_price(cafe_id):
         cafe.name = edit_form.name.data
         cafe.map_url = edit_form.map_url.data
         cafe.img_url = edit_form.img_url.data
-        cafe.location = edit_form.location.data
+        cafe.city = edit_form.city.data
         cafe.seats = edit_form.seats.data
         cafe.has_toilet = edit_form.has_toilet.data
         cafe.has_wifi = edit_form.has_wifi.data
         cafe.has_sockets = edit_form.has_sockets.data
         cafe.coffee_price = edit_form.coffee_price.data
         db.session.commit()
-        return jsonify(response={"Ok": "Successfully updated the cafe's data."}), 200, redirect(url_for("get_all"))
-    return jsonify(response={"Not Found": "The cafe's id was not found. Try using a valid id number."})
+        return redirect(url_for("get_all"))
+    return render_template("add_cafe.html", form=edit_form)
 
 
 @app.route("/delete/<int:cafe_id>")
@@ -166,8 +126,7 @@ def delete_cafe(cafe_id):
     if cafe_to_delete is not None:
         db.session.delete(cafe_to_delete)
         db.session.commit()
-        return jsonify(response={"Ok": "Successfully deleted the cafe."}), 200, redirect(url_for("home"))
-    return jsonify(response={"Not Found": "The cafe id was not found."}), 404
+    return redirect(url_for("get_all"))
 
 
 if __name__ == "__main__":
